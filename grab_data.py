3f20ce6d7e690d99d17785acb43088ecd6fca177
Lina Teckentrup <46699837+lteckentrup@users.noreply.github.com>
2022-09-07 11:54:53 +1000

    Update grab_data.py

--- grab_data.py
+++ grab_data.py
@@ -25,9 +25,10 @@ def readin_file(var,year,month):
 
     ### Filename
     fileIN = pathwayIN+var+'/'+year+'/'+var+'_era5-land_oper_sfc_'+year+month+'01-'+year+month+suffix
-
-    ### Read in as xarray dataset
-    ds = xr.open_dataset(fileIN)
+ 
+    ### Read in as xarray dataset. 
+    ### Files have scale and offset factors: set mask_and_scale=True
+    ds = xr.open_dataset(fileIN, mask_and_scale = True)
 
     return(ds)
 
