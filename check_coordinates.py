123f6add4423e972e27046f58dcebb14eda2de1c
Lina Teckentrup <46699837+lteckentrup@users.noreply.github.com>
2022-09-08 10:41:40 +1000

    Update check_coordinates.py

--- check_coordinates.py
+++ check_coordinates.py
@@ -2,24 +2,43 @@ import numpy as np
 import pandas as pd
 import xarray as xr
 
-### Generate years and months
-years = np.arange(1973,2022,1).astype(str)
-months = np.arange(1,13,1).astype(str)
-for i in range(0,10):
-    months[i] = months[i].zfill(2)
+'''
+Checks if right coordinates and years were extracted. Latitudes and longitudes
+were selected based on nearest match so I rounded values because they will not
+exactly match
+'''
 
-### Lopp through years and months    
-for year in years:
-    for month in months:
-        ### Open file
-        ds = xr.open_dataset('AT-Neu/tp_era5-land_oper_sfc_'+year+month+'.nc')
-        ### Check if right latitude
-        if ds.latitude.values != 47.099998474121094:
+def check_coordinates(var,site):
+    ### Generate years
+    years = np.arange(1973,2023,1).astype(str)
+    
+    ### Read in site information
+    df = pd.read_csv('sites.csv')
+    
+    ### Select site
+    df = df[(df['Site'] == site)]
+    
+    ### Select values for latitude and longitude
+    latitude = df['latitude'].values.tolist()[0]
+    longitude = df['longitude'].values.tolist()[0]
+    
+    for year in years:
+        ### Open site data
+        ds = xr.open_dataset(var+'/'+site+'/'+var+'_era5-land_oper_sfc_'+
+                             year+month+'.nc')
+        
+        ### Check if latitude in site data matches latitude in original csv
+        if round(ds.latitude.values.tolist(),1) != round(latitude,1):
             print('wrong latitude')
-         ### Check if right longitude
-        if ds.longitude.values != 11.300000190734863:
+        
+        ### Check if longitude in site data matches longitude in original csv
+        if round(ds.longitude.values.tolist(),1) != round(longitude,1):
             print('wrong longitude')
-        ### Check if right year
+         
+        ### Check if year in site data is correct
         for i in range(0,len(ds.time.dt.year.values)):
             if str(ds.time.dt.year.values[i]) != year:
                 print('wrong year')
+
+### E.g. check if total precipitation in AT-Neu is ok                
+check_coordinates('tp','AT-Neu')            
