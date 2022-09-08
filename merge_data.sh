f9efb2f797c1b603e198dd351ab5d3fefa19d548
Lina Teckentrup <46699837+lteckentrup@users.noreply.github.com>
2022-09-08 11:45:26 +1000

    Update merge_data.sh

--- merge_data.sh
+++ merge_data.sh
@@ -1,11 +1,12 @@
 #! /bin/bash
 
+### Define variable
+var='tp'
+
 ### Merge all data
 for d in */ ; do
-    cdo -L -b F64 mergetime "$d"* "$d"2t_era5-land_oper_sfc_1973-2022.nc
+    cdo -L -b F64 mergetime "$d"* "$d"${var}_era5-land_oper_sfc_1973-2022.nc
 done
 
-### Clean up: Only keep merged file
-for d in */ ; do
-    find . ! -name '"$d"2t_era5-land_oper_sfc_1973-2022.nc' -type f -exec rm -f {} +
-done
+### Clean up: Delete all files except merged 
+find . \! -name ${var}'_era5-land_oper_sfc_1973-2022.nc' -delete
