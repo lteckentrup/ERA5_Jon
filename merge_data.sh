3c88d5538fd3e0e94a2eb00cd0e3ffbe9a7b64e6
Lina Teckentrup <46699837+lteckentrup@users.noreply.github.com>
2022-09-08 11:05:55 +1000

    Update merge_data.sh

--- merge_data.sh
+++ merge_data.sh
@@ -5,7 +5,7 @@ for d in */ ; do
     cdo -L -b F64 mergetime "$d"* "$d"2t_era5-land_oper_sfc_1973-2022.nc
 done
 
-### Remove single years and only keep merged file
+### Clean up: Only keep merged file
 for d in */ ; do
     find . ! -name '"$d"2t_era5-land_oper_sfc_1973-2022.nc' -type f -exec rm -f {} +
 done
